11046a2d6cef870925271fb8de003ef59c7cfdd7
gamzekecibas <gkecibas16@ku.edu.tr>
2025-03-15 22:18:59 +0300

    Complete loops section

--- r-programming-fundamentals/intermediate-r.r
+++ r-programming-fundamentals/intermediate-r.r
@@ -103,3 +103,169 @@ sum(extremes)
 # } else {
 #   expression2
 # }
+
+# Variables related to your last day of recordings
+medium <- "LinkedIn"
+num_views <- 14
+
+# Control structure for medium
+if (medium == "LinkedIn") {
+  print("Showing LinkedIn information")
+} else if (medium == "Facebook") {
+  # Add code to print correct string when condition is TRUE
+  print("Showing Facebook information")
+} else {
+  print("Unknown medium")
+}
+
+# Control structure for num_views
+if (num_views > 15) {
+  print("You're popular!")
+} else if (num_views <= 15 & num_views > 10) {
+  # Add code to print correct string when condition is TRUE
+  print("Your number of views is average")
+} else {
+  print("Try to be more visible!")
+}
+
+# Variables related to your last day of recordings
+li <- 15
+fb <- 9
+
+# Code the control-flow construct
+if (li >= 15 & fb >=15) {
+  sms <- 2 * (li + fb)
+} else if (li < 10 & fb < 10) {
+  sms <- 0.5 * (li + fb)
+} else {
+  sms <- (li + fb)
+}
+
+# Print the resulting sms to the console
+print(sms)
+
+# While loop
+# Initialize the speed variable
+speed <- 64
+
+# Code the while loop
+while (speed > 30) {
+  print("Slow down!")
+  speed <- speed - 7
+}
+
+# Print out the speed variable
+print(speed)
+
+# Initialize the speed variable
+speed <- 64
+
+# Extend/adapt the while loop
+while (speed > 30) {
+  print(paste("Your speed is", speed))
+  if (speed > 48) {
+    print("Slow down big time!")
+    speed <- speed - 11
+  } else {
+    print("Slow down!")
+    speed <- speed - 6
+  }
+}
+
+# Initialize the speed variable
+speed <- 88
+
+while (speed > 30) {
+  print(paste("Your speed is", speed))
+  
+  if (speed > 80) {
+    break           # Break the while loop when speed exceeds 80
+  }
+  
+  if (speed > 48) {
+    print("Slow down big time!")
+    speed <- speed - 11
+  } else {
+    print("Slow down!")
+    speed <- speed - 6
+  }
+}
+
+# Initialize i as 1 
+i <- 1
+
+# Code the while loop
+while (i <= 10) {
+  print(3*i)
+  if ((i) %% 8 == 0) {
+    break
+  }
+  i <- i + 1
+}
+
+# For loop
+# The linkedin vector has already been defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Loop version 1: easily read but no access to indices
+for (li in linkedin){
+    print(li)
+}
+
+# Loop version 2: hard to read and write but easily access to indices
+for (i in 1:length(linkedin)){
+    print(linkedin[i])
+}
+
+# The nyc list is already specified
+nyc <- list(pop = 8405837, 
+            boroughs = c("Manhattan", "Bronx", "Brooklyn", "Queens", "Staten Island"), 
+            capital = FALSE)
+
+# Loop version 1
+for (n in nyc){
+    print(n)
+}
+
+# Loop version 2
+for (i in 1:length(nyc)){
+    print(nyc[[i]])
+}
+
+# The tic-tac-toe matrix ttt has already been defined for you
+
+# define the double for loop
+for (i in 1:nrow(ttt)) {
+  for (j in 1:ncol(ttt)) {
+    print(paste("On row", i, "and column", j, "the board contains", ttt[i, j]))
+  }
+}
+
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Adapt/extend the for loop
+for (li in linkedin) {
+  if (li > 10) {
+    print("You're popular!")
+  } else {
+    print("Be more visible!")
+  }
+  
+  # Add if statement with break
+  if (li > 16){
+    print("This is ridiculous, I'm outta here!")
+    break
+  }
+
+  # Add if statement with next
+  if (li < 5){
+    print("This is too embarrassing!")
+    next
+  }
+
+  print(li)
+}
+
+if (char == "r"){
+        rcount <- rcount + 1
+    }
